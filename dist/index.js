33283fc47e91b802da8bfa19f0767b8dc78f570e
Rory Martin <rorymartin3d@gmail.com>
2023-03-04 22:21:46 +0000

    Nav Bar Created

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+// Grab Everything we need
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+// Add Event Listeners
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#233554",
+          DEFAULT: "#112240",
+          dark: "#0A192F",
+          green: "#64FFDA",
+          pink: "#EF476F",
+          purple: "#D761FF",
+          purpleAlt: "#DF5FFF",
+          powderBlue: "#6977FF",
+        } ,     
+      },
+    },
   },
   plugins: [],
 }
