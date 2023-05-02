db9006350a84ef33c39379321b6c1c4420d0d84a
Rory Martin <rorymartin3d@gmail.com>
2023-05-02 15:18:47 +0100

    Code Style Changes

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 align="center">My Portfolio</h1>
+
+<p align="center">A page to detail my achievements and employment history</p>
+
+## Development
+
+```sh
+$ git clone git@github.com:roryhmartin/rorymartin.git
+$ cd rorymartin
+$ npm run dev
+```
--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 // Mobile Nav
 // Grab Everything we need
-const btn = document.querySelector("button.mobile-menu-button");
-const menu = document.querySelector(".mobile-menu");
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
 
 // Add Event Listeners
-btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
 });
 
 // Greeting Message
 // Set Up Variables
-const greeting = document.getElementById("greeting");
+const greeting = document.getElementById('greeting');
 const hours = new Date().getHours();
-const welcomeTypes = ["Good Morning", "Good Afternoon", "Good Evening"];
-let welcomeText = "";
+const welcomeTypes = ['Good Morning', 'Good Afternoon', 'Good Evening'];
+let welcomeText = '';
 
 // Create Message Logic
 if (hours < 12) {
@@ -24,25 +24,25 @@ if (hours < 12) {
     welcomeText = welcomeTypes[2];
 }
 // Use Logic on page
-greeting.innerHTML = welcomeText + ", my name is";
+greeting.innerHTML = welcomeText + ', my name is';
 
 // Hide & Show Panels
 
-const sections = document.querySelectorAll(".hide-section");
+const sections = document.querySelectorAll('.hide-section');
 sections.forEach((section) => {
-    section.addEventListener("click", function (event) {
+    section.addEventListener('click', function (event) {
         const cont = section.parentNode.nextElementSibling;
         const chevronDown = section.firstElementChild;
         const chevronUp = section.lastElementChild;
 
-        if (cont.classList.contains("block")) {
-            cont.classList.replace("block", "hidden");
-            chevronUp.classList.add("hidden");
-            chevronDown.classList.remove("hidden");
-        } else if (cont.classList.contains("hidden")) {
-            cont.classList.replace("hidden", "block");
-            chevronDown.classList.add("hidden");
-            chevronUp.classList.remove("hidden");
+        if (cont.classList.contains('block')) {
+            cont.classList.replace('block', 'hidden');
+            chevronUp.classList.add('hidden');
+            chevronDown.classList.remove('hidden');
+        } else if (cont.classList.contains('hidden')) {
+            cont.classList.replace('hidden', 'block');
+            chevronDown.classList.add('hidden');
+            chevronUp.classList.remove('hidden');
         }
     });
 });
